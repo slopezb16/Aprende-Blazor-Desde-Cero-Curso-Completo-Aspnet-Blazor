aee556685ca745e8fdebe048ae2b24f65c0c7065
slopezb16 <79761738+slopezb16@users.noreply.github.com>
2025-09-07 20:44:51 -0500

    feat: Sección 7: Base de Datos

--- BlazorExpenseTracker/BlazorExpenseTracker.Api/Program.cs
+++ BlazorExpenseTracker/BlazorExpenseTracker.Api/Program.cs
@@ -1,3 +1,5 @@
+using BlazorExpenseTracker.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configurar la conexion a la base de datos
+var sqlConnectionConfiguration = new SqlConfiguration(builder.Configuration.GetConnectionString("SqlConnection"));
+builder.Services.AddSingleton(sqlConnectionConfiguration);
 
 var app = builder.Build();
 
---
+++ BlazorExpenseTracker/BlazorExpenseTracker.Data/SqlConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorExpenseTracker.Data
+{
+    public class SqlConfiguration
+    {
+
+        public SqlConfiguration(string connectionString) => ConnectionString = connectionString;
+
+        public string ConnectionString { get; }
+    }
+}
